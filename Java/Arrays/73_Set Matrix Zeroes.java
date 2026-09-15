2b66bd561b3d320de5bd80021e0d9ffd4b43e8e9
haripriya2304 <haripriyavenkatesh07@gmail.com>
2026-09-15 11:06:06 +0530

    Create 73_Set Matrix Zeroes.java

---
+++ Java/Arrays/73_Set Matrix Zeroes.java
@@ -0,0 +1,31 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+
+        int r = matrix.length;
+        int c = matrix[0].length;
+
+        int[] rows = new int[r];
+        int[] cols = new int[c];
+
+        
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+
+                if (matrix[i][j] == 0) {
+                    rows[i] = 1;
+                    cols[j] = 1;
+                }
+            }
+        }
+
+        
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+
+                if (rows[i] == 1 || cols[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
